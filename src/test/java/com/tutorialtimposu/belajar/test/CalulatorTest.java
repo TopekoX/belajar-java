db30e45e37b6f3f01098652b494414885e660583
Ucup Topekox <ucup.topekox@gmail.com>
2022-04-10 17:10:33 +0800

    add Generator Name Test

--- src/test/java/com/tutorialtimposu/belajar/test/CalulatorTest.java
+++ src/test/java/com/tutorialtimposu/belajar/test/CalulatorTest.java
@@ -1,16 +1,19 @@
 package com.tutorialtimposu.belajar.test;
 
+import com.tutorialtimposu.belajar.test.generator.SimpleDisplayNameGenerator;
+import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 
-@DisplayName("Test untuk Calculator")
+//@DisplayName("Test untuk Calculator")
+@DisplayNameGeneration(SimpleDisplayNameGenerator.class)
 public class CalulatorTest {
 
     private Calculator calculator = new Calculator();
 
     @Test
-    @DisplayName("Test Skenario sukses untuk methid add(integer, integer)")
+//    @DisplayName("Test Skenario sukses untuk methid add(integer, integer)")
     public void testAddSuccess() {
         Integer result = calculator.add(10, 10);
 
@@ -24,7 +27,7 @@ public class CalulatorTest {
     }
 
     @Test
-    @DisplayName("Test Skenario throw exception yang diinginkan (tidak error)")
+//    @DisplayName("Test Skenario throw exception yang diinginkan (tidak error)")
     public void testDivideSuccess() {
         assertThrows(IllegalArgumentException.class, () -> {
             calculator.devide(100, 0);
