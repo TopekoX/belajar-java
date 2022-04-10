38816cdb13ee8475581f6b6a8d6e4e0c42ded503
Ucup Topekox <ucup.topekox@gmail.com>
2022-04-10 17:42:03 +0800

    lifecycle Unit Test before and after

--- src/test/java/com/tutorialtimposu/belajar/test/CalulatorTest.java
+++ src/test/java/com/tutorialtimposu/belajar/test/CalulatorTest.java
@@ -1,17 +1,40 @@
 package com.tutorialtimposu.belajar.test;
 
 import com.tutorialtimposu.belajar.test.generator.SimpleDisplayNameGenerator;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.DisplayName;
 
 //@DisplayName("Test untuk Calculator")
 @DisplayNameGeneration(SimpleDisplayNameGenerator.class)
 public class CalulatorTest {
 
     private Calculator calculator = new Calculator();
+
+    // @BeforeAll dan @AfterAll hanya dieksekusi satu
+    // kali saja di awal dan akhir class Test tetapi syaratnya
+    // method harus static
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Before All");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("After All");
+    }
+
+    // @BeforeEach dan @AfterEach akan dieksekusi pada semua
+    // method @Test
+    @BeforeEach
+    public void setUp() {
+        System.out.println("Before Each");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.out.println("After Each");
+    }
 
     @Test
 //    @DisplayName("Test Skenario sukses untuk methid add(integer, integer)")
