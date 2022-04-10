bb89894042fffd176cb104c001c32f877fc01b0c
Ucup Topekox <ucup.topekox@gmail.com>
2022-04-10 16:43:59 +0800

    AssertException "test gagal kalau tidak ada exception"

--- src/main/java/com/tutorialtimposu/belajar/test/Calculator.java
+++ src/main/java/com/tutorialtimposu/belajar/test/Calculator.java
@@ -2,6 +2,14 @@ package com.tutorialtimposu.belajar.test;
 
 public class Calculator {
 
+    public Integer devide(Integer first, Integer second) {
+        if(second == 0) {
+            throw new IllegalArgumentException("Tidak dapat dibagi 0");
+        } else {
+            return first / second;
+        }
+    }
+
     public Integer add (Integer first, Integer second) {
         return first + second;
     }
