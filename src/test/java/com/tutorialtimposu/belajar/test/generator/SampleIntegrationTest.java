a17d2d60d0e8024a5b429dc6598c37c4ad2cebc0
Ucup Topekox <ucup.topekox@gmail.com>
2022-04-12 09:47:10 +0800

    Tag

---
+++ src/test/java/com/tutorialtimposu/belajar/test/generator/SampleIntegrationTest.java
@@ -0,0 +1,24 @@
+package com.tutorialtimposu.belajar.test.generator;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+
+// tag: tanda
+// fungsinya ketika test kita bisa memilih class yang akan dijalankan
+// menjalankan dengan maven :
+// mvn test -Dgroups=tag1, tag2
+
+@Tags({
+        @Tag("integration-test")
+})
+public class SampleIntegrationTest {
+
+    @Test
+    void test1() {
+    }
+
+    @Test
+    void test2() {
+    }
+}
