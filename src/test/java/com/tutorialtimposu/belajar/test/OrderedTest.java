1ab440c05ab2084bc295a8b383d2d3605c478308
Ucup Topekox <ucup.topekox@gmail.com>
2022-04-12 11:22:30 +0800

    Lifecycle Test

--- src/test/java/com/tutorialtimposu/belajar/test/OrderedTest.java
+++ src/test/java/com/tutorialtimposu/belajar/test/OrderedTest.java
@@ -1,25 +1,48 @@
 package com.tutorialtimposu.belajar.test;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
+// Test instance PER_CLASS akan memberikan instance per class
+// secara default instancenya PER_METHOD
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class OrderedTest {
+
+    // contoh PER_METHOD
+    // OrderedTest o = new OrderedTest();
+    // o.test3();
+    // OrderedTest o = new OrderedTest();
+    // o.test2();
+    // OrderedTest o = new OrderedTest();
+    // o.test1();
+
+    // contoh PER_CLASS
+    // OrderedTest o = new OrderedTest();
+    // o.test3();
+    // o.test2();
+    // o.test1();
+
+    private int counter = 0;
 
     @Test
     @Order(2)
     void test3() {
+        counter++;
+        System.out.println(counter);
     }
 
     @Test
     @Order(1)
     void test2() {
+        counter++;
+        System.out.println(counter);
     }
 
     @Test
     @Order(3)
     void test1() {
+        counter++;
+        System.out.println(counter);
     }
 }
