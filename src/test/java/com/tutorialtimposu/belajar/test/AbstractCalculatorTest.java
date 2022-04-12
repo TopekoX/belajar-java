c89be5334ed6c6b9a96b410deecaf97ec09c344c
Ucup Topekox <ucup.topekox@gmail.com>
2022-04-13 02:45:14 +0800

    Pewarisan

---
+++ src/test/java/com/tutorialtimposu/belajar/test/AbstractCalculatorTest.java
@@ -0,0 +1,18 @@
+package com.tutorialtimposu.belajar.test;
+
+import com.tutorialtimposu.belajar.test.resolver.RandomParameterResolver;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.Extensions;
+
+@Extensions({
+        @ExtendWith(RandomParameterResolver.class)
+})
+public class AbstractCalculatorTest {
+        protected Calculator calculator = new Calculator();
+
+        @BeforeEach
+        void setUp() {
+                System.out.println("Before Each");
+        }
+}
