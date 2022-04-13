6682a5607a565bf5a9d605ded84a3252d91752a3
Ucup Topekox <ucup.topekox@gmail.com>
2022-04-13 18:59:32 +0800

    method berparameter source

--- src/test/java/com/tutorialtimposu/belajar/test/RandomCalculatorTest.java
+++ src/test/java/com/tutorialtimposu/belajar/test/RandomCalculatorTest.java
@@ -1,7 +1,11 @@
 package com.tutorialtimposu.belajar.test;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
 import java.util.Random;
 
 public class RandomCalculatorTest extends AbstractCalculatorTest {
@@ -46,6 +50,30 @@ public class RandomCalculatorTest extends AbstractCalculatorTest {
 
         var result = calculator.add(a, b);
         var expected = a + b;
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @DisplayName("Test calculator")
+    @ParameterizedTest(name = "{displayName} dengan Parameter {0}")
+    @ValueSource(ints = {1, 3, 4, 6, 77, 87, 88, 43, 66})
+    void testWithParameter(int value) {
+        var expected = value + value;
+        var result = calculator.add(value, value);
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    public static List<Integer> parameterSource() {
+        return List.of(1, 2, 4, 65, 78, 87, 43);
+    }
+
+    @DisplayName("Test calculator")
+    @ParameterizedTest(name = "{displayName} dengan Parameter {0}")
+    @MethodSource({"parameterSource"})
+    void testWithMethodSource(Integer value) {
+        var expected = value + value;
+        var result = calculator.add(value, value);
 
         Assertions.assertEquals(expected, result);
     }
