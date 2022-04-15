fd0f0e71d1745bcce23993f3d7e129775b58bc22
Ucup Topekox <ucup.topekox@gmail.com>
2022-04-16 03:26:29 +0800

    Interface Comparator

---
+++ java-generic/src/main/java/com/tutorialtimposu/belajar/application/ComparatorApp.java
@@ -0,0 +1,29 @@
+package com.tutorialtimposu.belajar.application;
+
+import com.tutorialtimposu.belajar.Person;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ComparatorApp {
+    public static void main(String[] args) {
+
+        Person[] people = {
+                new Person("Budi","Indonesia"),
+                new Person("Ucup","Indonesia"),
+                new Person("Azwar","Indonesia")
+        };
+
+        Comparator<Person> comparator = new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        };
+
+        Arrays.sort(people, comparator);
+
+        System.out.println(Arrays.toString(people));
+
+    }
+}
