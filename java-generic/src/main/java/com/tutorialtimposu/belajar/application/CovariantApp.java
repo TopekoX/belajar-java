fe0556bc9fe036ed1576b5e89efc344b6b6c68d9
Ucup Topekox <ucup.topekox@gmail.com>
2022-04-16 01:10:20 +0800

    Covariant

---
+++ java-generic/src/main/java/com/tutorialtimposu/belajar/application/CovariantApp.java
@@ -0,0 +1,23 @@
+package com.tutorialtimposu.belajar.application;
+
+import com.tutorialtimposu.belajar.MyData;
+
+public class CovariantApp {
+    public static void main(String[] args) {
+
+        MyData<String> stringMyData = new MyData<>("Ucup");
+
+        // cara 1
+        process(stringMyData);
+
+        // cara 2
+        MyData<? extends Object> myData = stringMyData;
+
+    }
+
+    public static void process(MyData<? extends Object> myData) {
+        System.out.println(myData.getData());
+
+        //myData.getData("Halo"); // tidak bisa panggil get
+    }
+}
