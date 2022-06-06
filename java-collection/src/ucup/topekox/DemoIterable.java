38546ecdff3481c6c8cc5af11207916ec23737bc
Ucup TopekoX <ucup.topekox@gmail.com>
2022-06-06 16:34:34 +0800

    Iterable

--- java-collection/src/ucup/topekox/DemoIterable.java
+++ java-collection/src/ucup/topekox/DemoIterable.java
@@ -3,6 +3,25 @@ package ucup.topekox;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * Tidak ada magic di Java, sebenarnya for-each di Iterable bisa terjadi karena ada method iterator() yang mengembalikan object Iterator
+ * Iterator adalah interface yang mendefinisikan cara kita melakukan mengakses element di collection secara sequential
+ * For-each sendiri muncul sejak Java 5, sebelum Java 5 untuk melakukan iterasi collection, biasanya dilakukan manual menggunakan Iterator object
+ */
+
+/**
+ * Iterable adalah parent untuk semua collection di Java, kecuali Map
+ * Iterable sendiri sangat sederhana, hanya digunakan agar mendukung for-each loop
+ * Karena semua collection pasti implement iterable, secara otomatis maka semua collection di Java mendukung perulangan for-each, jadi bukan cuma Array
+ */
+
+/**
+ * Selain Iterable interface, parent class semua collection di Java adalah Collection
+ * Kalo Iterable interface digunakan sebagai kontrak untuk meng-iterasi data secara sequential
+ * Collection merupakan kontrak untuk memanipulasi data collection, seperti menambah, menghapus dan mengecek isi data collection
+ * Tidak ada direct implementation untuk Collection, karena collection nanti akan dibagi lagi menjadi List, Set dan Queue
+ */
+
 public class DemoIterable {
     public static void main(String[] args) {
 
