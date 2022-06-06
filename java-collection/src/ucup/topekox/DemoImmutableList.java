0ba66e9f551b20d9a6b0d039348da88008c14f21
Ucup TopekoX <ucup.topekox@gmail.com>
2022-06-06 15:52:43 +0800

    List

--- java-collection/src/ucup/topekox/DemoImmutableList.java
+++ java-collection/src/ucup/topekox/DemoImmutableList.java
@@ -4,6 +4,25 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Secara default, List di Java baik itu ArrayList ataupun LinkedList datanya bersifat mutable (Bisa diubah)
+ * Di Java mendukung pembuatan Immutable List, artinya datanya tidak bisa diubah lagi.
+ * Sekali List dibuat, datanya tidak bisa diubah lagi, alias final.
+ * Tapi ingat, data list nya yang tidak bisa diubah, bukan reference object element nya.
+ * Kalo misal kita membuat Immutable List berisikan data Person, field data Person tetap bisa diubah,
+ * tapi isi elemen dari Immutable List tidak bisa diubah lagi
+ * Ini cocok ketika kasus-kasus misal, tidak sembarangan code yang boleh mengubah element di List
+ */
+
+/**
+ *  Method                                  |       Keterangan
+ *  ----------------------------------------------------------------------------
+ * Collections.emptyList()                  |       Membuat immutable list kosong
+ * Collections.singletonList(e)             |       Membuat immutable list berisi 1 element
+ * Collections.unmodifiableList(list)       |       Mengubah mutable list menjadi immutable
+ * List.of(e...)                            |       Membuat immutable list dari element - element
+ */
+
 public class DemoImmutableList {
     public static void main(String[] args) {
         List<String> one = Collections.singletonList("Satu");
--- java-collection/src/ucup/topekox/DemoList.java
+++ java-collection/src/ucup/topekox/DemoList.java
@@ -5,6 +5,27 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
+ * List adalah struktur data collection yang memiliki sifat sebagai berikut
+ * Elemen  di list bisa duplikat, artinya bisa memasukkan data yang sama
+ * Data list berurut sesuai dengan posisi kita memasukkan data
+ * List memiliki index, sehingga kita bisa menggunakan nomor index untuk mendapatkan element di list
+ * Di Java ada beberapa implementasi List, dan kita bisa memilih sesuai dengan kebutuhan kita
+ */
+
+/**
+ * ArrayList adalah implementasi dari List menggunakan array
+ * Default kapasitas array di ArrayList adalah 10
+ * Namun ketika kita memasukkan data dan array sudah penuh,
+ * maka secara otomatis ArrayList akan membuat array baru dengan
+ * kapasitas baru dengan ukuran kapasitas lama + data baru
+ */
+
+/**
+ * LinkedList adalah implementasi List dengan struktur data Double Linked List
+ * Data yang dimasukan akan berurut
+ */
+
+/**
  * List = Data yang ditampung diindex seperti array
  * ArrayList vs LinkedList
  * ArrayList = menampung datanya berdasarkan array
@@ -16,6 +37,7 @@ import java.util.List;
  * get              | cepat                                   | lambat
  * remove           | lambat harus menggeser index            | cepat
  */
+
 public class DemoList {
 
     public static void main(String[] args) {
